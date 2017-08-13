55630373db046874178064486c5a954db959e1a1
Joseph Pan <m@hahack.com>
2017-08-13 14:54:19 +0800

    增加音效

--- switcher.py
+++ switcher.py
@@ -4,6 +4,9 @@
 from gpiozero import Button
 import alsaaudio
 import time
+import sys
+import os
+import subprocess
 
 button = Button(17)
 
@@ -20,9 +23,11 @@ while True:
         if (now - last) > 0.5:
             if mixer.getrec() == [1, 1]:
                 print('mic is disabled')
-                mixer.setrec(0)
+                mixer.setrec(0)                
+                subprocess.call(["play", os.path.join(sys.path[0], "sounds", "match2.wav")])
             else:
                 print('mic is enabled')
                 mixer.setrec(1)
+                subprocess.call(["play", os.path.join(sys.path[0], "sounds", "match1.wav")])                
             last = now
     time.sleep(0.010)
